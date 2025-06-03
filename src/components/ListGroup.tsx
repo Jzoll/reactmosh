3fb36fc6fc33b99b873ef22703e3e6345964f187
Jzoll <hellojzoll@gmail.com>
2025-06-03 17:11:06 -0400

    mosh tutorial progress

---
+++ src/components/ListGroup.tsx
@@ -0,0 +1,24 @@
+function ListGroup() {
+  let items = ["L", "A", "M", "B"];
+
+  return (
+    <>
+      <h1>List</h1>
+      {/* old way {items.length === 0 ? <p>No item found</p> : null} */}
+      {items.length === 0 && <p>No item found</p>}
+      <ul className="list-group">
+        {items.map((item, index) => (
+          <li
+            className="list-group-item"
+            key={item}
+            onClick={(event) => console.log(event)}
+          >
+            {item}
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+}
+
+export default ListGroup;
